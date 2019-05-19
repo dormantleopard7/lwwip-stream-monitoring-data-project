00e5e1dd5af1876657b0d050017e7a81383419ad
Anirudh Prakash <anipra7@gmail.com>
2019-05-18 22:59:24 -0700

    refactoring within Main, formatting with text interface
    menu to write or analyze, added a notes file, changed readme a bit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# lwwip-stream-monitoring-data-project
+# LWWIP Stream-Monitoring Data Project
 
 As part of my 3rd year at the Lake Washington Watershed Internship Program, I have chosen to do a technical project, particularly analyzing (and perhaps visualizing) our stream monitoring data.
 
-This project began in November 2018, and was added to GitHub in March 2019.
+This project began in November 2018, and was added to GitHub in March 2019. I intend to finish by June 2019.
--- src/main/StreamMonitoringMain.java
+++ src/main/StreamMonitoringMain.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -11,101 +10,82 @@ import static main.StreamMonitoringDataModel.*;
 public class StreamMonitoringMain {
     public static final String STREAM_FILE_PATH = "src/main/data/coal_creek_data.tsv";
 
-    public static void main(String[] args) throws FileNotFoundException {
-        // Notes:
-        // - double quotes character on row 305/6 (it was a typo anyway)
-        // - might need any boxes with just notes and no numbers in data columns to be gone
-        //   (like AQ:282 "Site 2 too high...")
-        // - changed 1.2m to 10NTU at G:284 and G:285
-        // Using BindByName (old):
-        // - to preserve column names, need to change the turbidity column names
-        //   to distinguish between the two
-        // - ideal to put space after degree symbol in column S
-        //      or I could figure out regexes
-        // - also ideal to change AQ column header
-        // Using BindByPosition (new):
-        // - remove headers altogether
-
+    public static void main(String[] args) {
         StreamMonitoringDataModel streamModel = new StreamMonitoringDataModel(STREAM_FILE_PATH);
 
-        //StreamMonitoringDataWriter.write(streamModel, "data.tsv");
-
-        // options:
-        //  start and end date
-        //  site 1 or 2
-        //  data type
-        //  also should be able to check a particular date and entry and examine it
-        // things to see (*data types):
-        // - date
-        // - site
-        // - coordinates
-        // - sheet #
-        // * turbidity (basically fixed)
-        // * air temp (should be good to go)
-        // * water temp (should be good to go)
-        // * pH (should be good)
-        // * DO (should be good)
-        // * flow (needs fixing)
-        // - phosphate
-        // - nitrate/nitrite
-        // * conductivity (should be good)
-        // - rain
-        // - benthic macro
-        // - data recorder
-        // - notes
-
+        printMenu();
         Scanner console = new Scanner(System.in);
-        String resp = "continue";
-        while (!resp.equalsIgnoreCase("q")) {
+        String option;
+        do {
+            System.out.println();
+            System.out.print("Enter an option (m to see the menu): ");
+            option = console.nextLine();
+            processOption(option, streamModel, console);
+        } while (!option.equalsIgnoreCase("q"));
+
+        // Future:
+        // - need to fix flow units stuff
+        // - visualizing (histogram, scatter plot, line graph, bar graph, pie chart)
+    }
+
+    public static void processOption(String option, StreamMonitoringDataModel streamModel, Scanner console) {
+        if (option.equalsIgnoreCase("m")) {
+            printMenu();
+        } else if (option.equalsIgnoreCase("w")) {
+            System.out.print("Output file name: ");
+            String outputFileName = console.nextLine();
+            StreamMonitoringDataWriter.write(streamModel, outputFileName);
+        } else if (option.equalsIgnoreCase("a") || option.equalsIgnoreCase("s")) {
             System.out.print("Start date: ");
             Date start = new Date(console.nextLine());
             System.out.print("End date: ");
             Date end = new Date(console.nextLine());
             System.out.print("Site (1 or 2; 0 if want both): ");
             int site = Integer.parseInt(console.nextLine());
-            /*System.out.println("Data type: " +
-                    "(1) Turbidity (NTU), (2) Turbidity (m), " +
-                    "(3) Air Temperature (°C), (4) Water Temperature (°C), " +
-                    "(5) pH, (6) Dissolved Oxygen (ppm), (7) Conductivity (μS/cm)");*/
             System.out.print("Data type (" + DATA_TYPES + "): ");
             int dataType = Integer.parseInt(console.nextLine());
-
-            //System.out.println("Average: " + streamModel.getMean(dataType, start, end));
-
-            System.out.println();
-            System.out.println("Resulting Statistics");
-            System.out.println("--------------------");
             List<Double> sortedData = streamModel.getData(dataType, site, start, end);
-            double mean = streamModel.getMean(sortedData);
-            double stdDev = streamModel.getStdDev(sortedData, mean);
-            List<Double> modes = streamModel.getMode(sortedData);
-            int modeFreq = (int) (double) modes.get(modes.size() - 1);
-            modes.remove(modes.size() - 1);
-            double min = streamModel.getMin(sortedData);
-            double[] quartiles = streamModel.getQuartiles(sortedData);
-            double max = streamModel.getMax(sortedData);
-            List<Double> outliersStdDev = streamModel.getOutliersStdDev(sortedData, mean, stdDev);
-            List<Double> outliersIQR = streamModel.getOutliersIQR(sortedData, quartiles);
-            System.out.println("Mean: " + mean);
-            System.out.println("Standard Deviation: " + stdDev);
-            if (modes.isEmpty()) {
-                System.out.println("Mode (frequency 1): all data unique");
-            } else {
-                System.out.println("Mode (frequency " + modeFreq + "): " + modes);
-            }
-            double[] boxplot = new double[] { min, quartiles[0], quartiles[1], quartiles[2], max };
-            System.out.println("Min, Q1, Median, Q3, Max: " + Arrays.toString(boxplot));
-            System.out.println("Outliers (based on " + OUTLIER_SDS + " std devs): " + outliersStdDev);
-            System.out.println("Outliers (based on " + OUTLIERS_IQR + " IQRs): " + outliersIQR);
-            System.out.println();
 
-            System.out.print("Continue? (Type q to quit) ");
-            resp = console.nextLine();
             System.out.println();
+            printStats(streamModel, sortedData);
+        } else if (!option.equalsIgnoreCase("q")) {
+            System.out.println("Unknown option");
         }
+    }
 
-        // Future:
-        // - need to fix flow units stuff
-        // - visualizing (histogram, scatter plot, line graph, bar graph, pie chart)
+    public static void printMenu() {
+        System.out.println("Menu:");
+        System.out.println("\tw to write cleaned data to file");
+        System.out.println("\ta to analyze data statistically");
+        //System.out.println("\tv to visualize the data");
+        System.out.println("\tq to quit");
+    }
+
+    public static void printStats(StreamMonitoringDataModel streamModel, List<Double> sortedData) {
+        System.out.println("Resulting Statistics");
+        System.out.println("--------------------");
+
+        double mean = streamModel.getMean(sortedData);
+        double stdDev = streamModel.getStdDev(sortedData, mean);
+        List<Double> modes = streamModel.getMode(sortedData);
+        int modeFreq = (int) (double) modes.get(modes.size() - 1);
+        modes.remove(modes.size() - 1);
+        double min = streamModel.getMin(sortedData);
+        double[] quartiles = streamModel.getQuartiles(sortedData);
+        double max = streamModel.getMax(sortedData);
+        List<Double> outliersStdDev = streamModel.getOutliersStdDev(sortedData, mean, stdDev);
+        List<Double> outliersIQR = streamModel.getOutliersIQR(sortedData, quartiles);
+
+        System.out.println("Mean: " + mean);
+        System.out.println("Standard Deviation: " + stdDev);
+        if (modes.isEmpty()) {
+            System.out.println("Mode (frequency 1): all data unique");
+        } else {
+            System.out.println("Mode (frequency " + modeFreq + "): " + modes);
+        }
+        double[] boxplot = new double[] { min, quartiles[0], quartiles[1], quartiles[2], max };
+        System.out.println("Min, Q1, Median, Q3, Max: " + Arrays.toString(boxplot));
+        System.out.println("Outliers (based on " + OUTLIER_SDS + " std devs): " + outliersStdDev);
+        System.out.println("Outliers (based on " + OUTLIERS_IQR + " IQRs): " + outliersIQR);
     }
 }
